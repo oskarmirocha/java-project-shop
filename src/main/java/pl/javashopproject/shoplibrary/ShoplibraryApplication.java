609b9e0cc1244f3498414061a54cb99c1ee3eae4
oskarmirocha <oskar.mirocha@microsoft.wsei.edu.pl>
2022-09-06 11:07:10 +0200

    Initial commit

---
+++ src/main/java/pl/javashopproject/shoplibrary/ShoplibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.javashopproject.shoplibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoplibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/javashopproject/shoplibrary/ShoplibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javashopproject.shoplibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
