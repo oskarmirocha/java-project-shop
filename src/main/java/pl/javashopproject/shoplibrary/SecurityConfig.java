0f62ed8be725e0eda57644ceac16025e7b562b78
oskarmirocha <oskar.mirocha@microsoft.wsei.edu.pl>
2022-09-07 01:46:58 +0200

    Security Configuration

---
+++ src/main/java/pl/javashopproject/shoplibrary/SecurityConfig.java
@@ -0,0 +1,39 @@
+package pl.javashopproject.shoplibrary;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+        return http.build();
+    }
+
+    @Bean
+    @Deprecated
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
