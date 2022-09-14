152d0e33bbdbc896873dbe73efa72ecffd43838e
oskarmirocha <oskar.mirocha@microsoft.wsei.edu.pl>
2022-09-14 10:57:00 +0200

    /addMessage RabbitMQ

--- src/main/java/pl/javashopproject/shoplibrary/PublisherMq.java
+++ src/main/java/pl/javashopproject/shoplibrary/PublisherMq.java
@@ -1,20 +1,21 @@
 package pl.javashopproject.shoplibrary;
 
-import ch.qos.logback.classic.pattern.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PublisherMq {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @GetMapping
+    @GetMapping("/addMessage")
     public String get(@RequestParam String message){
-        rabbitTemplate.convertAndSend("message", message);
-        return "send";
+        rabbitTemplate.convertAndSend("erty", message);
+        return "sent";
 
     }
 
