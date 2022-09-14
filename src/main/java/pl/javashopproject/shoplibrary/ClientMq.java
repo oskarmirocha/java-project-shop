f81009e9b4d3ef70f33c47de1d5ff20c3a383e1a
oskarmirocha <oskar.mirocha@microsoft.wsei.edu.pl>
2022-09-14 11:16:55 +0200

    /receiveMessage RabbitMQ

---
+++ src/main/java/pl/javashopproject/shoplibrary/ClientMq.java
@@ -0,0 +1,19 @@
+package pl.javashopproject.shoplibrary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientMq {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/receiveMessage")
+    public String get(){
+        Object message = rabbitTemplate.receiveAndConvert("erty");
+        return message.toString();
+    }
+}
